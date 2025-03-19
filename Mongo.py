7c4443c3f2eaf66caa6b4ebeb5323c315a04816f
Jose-Maria-Caracuel <josemariacaracuelmenacho@gmail.com>
2025-03-19 18:16:33 +0100

    pagina monstruos

--- Mongo.py
+++ Mongo.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import json
-import os
 
 
 ListasURL = ["https://mhw-db.com/monsters", "https://mhw-db.com/armor", "https://mhw-db.com/weapons",
                 "https://mhw-db.com/skills", "https://mhw-db.com/items"]
 
 try:
+<<<<<<< Updated upstream
     client = MongoClient("mongodb://localhost:27017/")
     monsters = json.load(open("monsters.json"))
     database = client['MH_Wiki']
@@ -20,6 +20,27 @@ try:
     #database['Habilidad'].insert_one({"nombre": "Resistencia al fuego", "nivel": 3})
     #database['Objeto'].insert_one({"nombre": "Poción", "efecto": "Recupera salud"})
     #database['Usuarios'].insert_one({"username": "Hunter01", "rango": "Alto"})
+=======
+    for i in ListaJSON:
+        print(f"Creando base de datos de {i}")
+        client = MongoClient("mongodb://localhost:27017")
+        print("Conexión exitosa")
+        monsters = json.load(open(i+".json"))
+        print("Cargando datos")
+        database = client['MH_Wiki']
+        print("Base de datos cargada")
+
+        # Luego creo la api y la meto en las tablas y creo el script para que se ejecute en el servidor 
+        for monster in monsters:
+            print(f"Insertando {monster['name']} en la base de datos")
+            database[i].insert_one(monster)
+        # os.remove("monsters.json")
+    # database['Armadura'].insert_one({"nombre": "Armadura Rathalos", "defensa": 50})
+        #database['Arma'].insert_one({"nombre": "Espada Llamarada", "ataque": 120})
+        #database['Habilidad'].insert_one({"nombre": "Resistencia al fuego", "nivel": 3})
+        #database['Objeto'].insert_one({"nombre": "Poción", "efecto": "Recupera salud"})
+        #database['Usuarios'].insert_one({"username": "Hunter01", "rango": "Alto"})
+>>>>>>> Stashed changes
 
 
     print("Bases de datos disponibles:", client.list_database_names())
